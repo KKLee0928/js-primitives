d1ef3004c23d190ac4c3c6d3212fcdc45af1714a
kklee0928 <kevinklee86@gmail.com>
2017-09-01 20:46:32 -0700

    javaScript exercises

---
+++ js/exercise1.js
@@ -0,0 +1,20 @@
+var friends = [
+  'Moe',
+  'Larry',
+  'Curly',
+  'Jane',
+  'Emma',
+  'Elizabeth',
+  'Elinor',
+  'Mary',
+  'Darcy',
+  'Grey',
+  'Lydia',
+  'Harriet'
+];
+
+console.log(friends.sort(' '));
+
+var friends = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
+var arrFriends = [];
+
